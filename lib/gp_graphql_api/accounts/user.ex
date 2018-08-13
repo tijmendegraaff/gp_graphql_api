18f80cd69f9b66d924197d8a5a635e372ab754d1
Tijmen de Graaff <tijmen76@gmail.com>
2018-08-13 18:45:40 +0200

    add model validation and put password into password_hash

--- lib/gp_graphql_api/accounts/user.ex
+++ lib/gp_graphql_api/accounts/user.ex
@@ -4,9 +4,13 @@ defmodule GpGraphqlApi.Accounts.User do
 
 
   schema "users" do
-    field :email, :string
-    field :password_hash, :string
-    field :role, :string
+    field(:email, :string, unique: true)
+    field(:password_hash, :string)
+    field(:role, :string)
+    
+    # Virtual fields
+    field(:password, :string, virtual: true)
+    field(:password_confirmation, :string, virtual: true)
 
     timestamps()
   end
@@ -14,7 +18,23 @@ defmodule GpGraphqlApi.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:email, :password_hash, :role])
-    |> validate_required([:email, :password_hash, :role])
+    |> cast(attrs, [:email, :role, :password, :password_confirmation])
+    |> validate_required([:email, :password, :password_confirmation, :role])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 6)
+    |> validate_confirmation(:password)
+    |> unique_constraint(:email)
+    |> hash_password
   end
+
+  # Hash password
+  defp hash_password(changeset) do
+    case changeset do
+    %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+      put_change(changeset, :password_hash, password)
+    _ ->
+      changeset
+    end
+  end
+
 end
