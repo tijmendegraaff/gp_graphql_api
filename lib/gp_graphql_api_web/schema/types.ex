2d2902fcf36e8dc399a0a7fe962e9479caa81381
Tijmen de Graaff <tijmen76@gmail.com>
2018-08-14 00:40:24 +0200

    create index types file

---
+++ lib/gp_graphql_api_web/schema/types.ex
@@ -0,0 +1,8 @@
+defmodule GpGraphqlApiWeb.Schema.Types do
+    use Absinthe.Schema.Notation
+
+    alias GpGraphqlApiWeb.Schema.Types
+
+    # import your types
+    import_types(Types.UserType)
+end
